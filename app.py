c23075dc7df0abe67880b5c5603d19775ec8d9da
Igor Oliveira <44407469+oliveiraigordev@users.noreply.github.com>
2024-04-01 23:52:56 -0300

    :sparkles: finalizado CRUD flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-'''Módulo utilizado para introdução ao Flask'''
+'''Módulo utilizado para estudos de introdução ao Flask'''
 import uuid
 from flask import Flask, request, jsonify
 from models.task import Task
@@ -12,9 +12,12 @@ tasks = []
 @app.route('/tasks', methods=['POST'])
 def create_task():
     data = request.get_json()
-    new_task = Task(id=uuid.uuid4(), title=data['title'], description=data.get('description',''))
+    new_task = Task(
+        id=uuid.uuid4(),
+        title=data['title'],
+        description=data.get('description', ''))
     tasks.append(new_task)
-    return jsonify({"message": "Nova tarefa criada com sucesso"})
+    return jsonify({"message": "Nova tarefa criada com sucesso!"})
 
 
 @app.route('/tasks', methods=['GET'])
@@ -29,10 +32,45 @@ def get_tasks():
 
 @app.route('/tasks/<uuid:id>', methods=['GET'])
 def get_task(id):
-    for task in tasks:
-        if task.id == id:
-            return jsonify(task.to_dict())
-    return jsonify({"message": "Não foi possível encontrar a tarefa"}), 404
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+    return jsonify({"message": "Não foi possível encontrar a tarefa."}), 404
+
+
+@app.route('/tasks/<uuid:id>', methods=['PUT'])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+    if task is None:
+        return jsonify(
+            {"message": "Não foi possível encontrar a tarefa."}
+            ), 404
+
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    return jsonify({"message": "Tarefa atualizada com sucesso!"})
+
+
+@app.route('/tasks/<uuid:id>', methods=['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+    if task is None:
+        return jsonify(
+            {"message": "Não foi possível encontrar a tarefa."}
+            ), 404
+
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa apagada com sucesso!"})
 
 
 app.run(debug=True)
